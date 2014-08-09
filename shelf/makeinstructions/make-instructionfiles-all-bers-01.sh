f992dcad5aba291c9b63f36487d75071c6296037
rblandau <landau@ricksoft.com>
2014-08-09 18:53:31 -0400

    Add shell scripts to run over copies and lifetime ranges.  Files: runsequencecopiesandlifetimes-base-00.sh, runsequencecopiesandlifetimes-fillout-00.sh, runallallall-00.sh. Revise makeinstructions template for Q0b runs with no auditing but more complete recording of okay, majority, minority, and lost. Add recent line counts, WeeklyCodeLineCounts-20140804b.txt.

---
+++ shelf/makeinstructions/make-instructionfiles-all-bers-01.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# make-instructionfiles-all-bers.sh
+# Create all the empty log dirs for a family/specific dir.
+#                   RBLandau 20140613
+
+if [ "$1" = "-h" ] || [ "$1" = "?" ] || [ -z "$3" ]
+then
+    echo "Usage: $0 <familyrootdir> <familydir> <specificdir>"
+    echo "Creates instruction files for all BER levels in familyroot/family/specific."
+    exit 1
+fi
+
+wholedir="$1/$2/$3"
+if [ ! -d "$wholedir" ]
+then
+    echo "ERROR: <$wholedir> is not a directory."
+    exit 1
+fi
+
+# Get the latest version of any files needed
+for ff in makeinstructionfiles NewTraceFac ; do
+    newffull=$(ls $ff*.py | sort | tail -1 | sed 's/\\r//');
+    cp -v $newffull $ff.py ;
+done
+
+# Create instruction files in the log dirs for all possible LIFEM values.
+#for lifem in 0001 0002 #0003 0005 0010 0020 0030 0050 0100 0200 0300 0500 1000 2000 3000 5000 10000
+for lifem in 0001 0002 0003 0005 0010 0020 0030 0050 0100 0200 0300 0500 1000 2000 3000 5000 10000
+do 
+    echo "python makeinstructionfiles.py --familyroot=$1 --family=$2 --specific=$3  makeinstructions-b$lifem.txt"
+    python makeinstructionfiles.py --familyroot=$1 --family=$2 --specific=$3  makeinstructions-b$lifem.txt
+
+done
+
+#END
