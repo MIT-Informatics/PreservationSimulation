8ae48d5fa836fdd26f4b183da6498b29f9131395
Richard Landau <landau@ricksoft.com>
2015-07-08 22:24:31 -0400

    Add R scripts.

---
+++ shelf/R/shelf9_call.r
@@ -0,0 +1,7 @@
+# Edit me and run me.  (Select all, then ctrl-Enter.)
+
+sInputFilename  <- "./GiantOutput_log_doc50_shlf10_af10000s1tTOTAL_gf10000icombinedd0m1000-output-00.txt"
+sOutputFilename <- "./q3-af10000s1-gf10000icombinedd0m1000-AnnualAudit-AnnualGlitchCombinedRates-analysis.txt"
+sTitle <- "Q3 Glitch Annual Combined Rates 1000hr max, no decay, Audit Annual TOTAL"
+source("./ShelfAnalyze-glitchimpact-04.r")
+
--- shelf/emptygiantoutput.sh
+++ shelf/emptygiantoutput.sh
@@ -1,7 +1,28 @@
 #!/bin/sh
 # emptygiantoutput.sh
 # Must be run from shelf directory.  
+# Defaults to Q3 family and . specific, but can be overridden by args 1 and 2
 
-familydir=../Q3
-specificdir=.
+if [ "$1" == "-h" ]
+then
+    echo "Usage: $0 [<familydir> [<specificdir>]]"
+    echo "Defaults to ../Q3 and ."
+    exit 1
+fi
+
+if [ -z "$1" ] 
+then
+    familydir=../Q3
+else
+    familydir="$1"
+fi
+if [ -z "$2" ] 
+then
+    specificdir=.
+else
+    specificdir="$2"
+fi
+
 cp -v $familydir/$specificdir/GiantOutput_HeaderOnly.txt $familydir/$specificdir/dat/GiantOutput_00.txt
+
+#END
