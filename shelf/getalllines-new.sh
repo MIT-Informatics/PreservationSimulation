704f8408a4d7c7570e61faa7dcc50f4e6ae11b00
rblandau <landau@ricksoft.com>
2015-07-14 19:40:00 -0400

    Ignore some more files

---
+++ shelf/getalllines-new.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# getalllines-new.sh
+
+if [ -z "$2" ]
+then
+    echo "Usage: $0 <directorytree of log files> <instructionsfile>"
+    exit 1
+fi
+
+if [ ! -d "$1" ]
+then
+    echo "Error: \"$1\" is not a directory."
+    exit 1
+fi
+
+# Instruction file now mandatory.
+instructions="$2"
+
+export header=1
+for filename in `find $1 -name '*.log' -print`
+do
+    #echo "for value $filename env value $header"
+    python extractvalues.py $instructions $filename
+    unset header
+done
