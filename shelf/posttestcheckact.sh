303eb348535e55fbd0e2dccf1d22219e2dd517fd
Richard Landau <landau@ricksoft.com>
2015-08-10 11:29:04 -0400

    Improve post-run test to check for non-empty /ext/ dir and mismatched /act/ and /log/ dirs.

--- shelf/posttestcheckact.sh
+++ shelf/posttestcheckact.sh
@@ -26,9 +26,27 @@ then
 else
     sSpecificDir="$2"
 fi
+sFullDir=$sFamilyDir/$sSpecificDir
 
+if [ 0 -lt $(ls $sFullDir/ext/ | wc -l) ]
+then
+    echo ""
+    echo "WARNING! The $sFullDir/ext/ is not empty, but should be."
+    echo ""
+fi
+
+if [ $(ls $sFullDir/act | wc -l) -ne $(ls $sFullDir/log | wc -l) ] 
+then
+    echo ""
+    echo "WARNING! The $sFullDir/act and .../log dirs contain different numbers of files."
+    echo ""
+fi
+
+echo "Check visually that the size range of ..._actor.log files is very narrow."
 ls -l $sFamilyDir/$sSpecificDir/act|awk '{print $5, "  ", $9 }'| sort  >  /tmp/posttestcheckact.txt
 head -6 /tmp/posttestcheckact.txt
+echo ""
+echo "  (smallest  ^^^    to    largest  vvv)"
 echo ""
 tail -5 /tmp/posttestcheckact.txt
 rm -f /tmp/posttestcheckact.txt
