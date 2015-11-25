c862ce0d139cc61e81c5b3a6b421eeab33e73bec
rblandau <landau@ricksoft.com>
2015-11-25 16:29:24 -0500

    Add memoize decorator

---
+++ shelf/newinstructions/memoize.py
@@ -0,0 +1,19 @@
+# memoize.py
+#
+# Fairly trivial memoize decorator for use with more or less any function.
+# Works with *args, **kwargs, and unhashable types.  
+# Perhaps not the most efficient, but fairly bulletproof.  
+# From PythonWiki.
+# With apologies to Clorox Bleachman, the actress not the drag queen.
+#                   RBLandau 20151124
+
+    import functools
+    def memoize(rememberme):
+        cache = {}
+        @functools.wraps(rememberme)
+        def memoizer(*args, **kwargs):
+            key = str(args) + str(kwargs)
+            if not key in cache:
+                cache[key] = rememberme(*args, **kwargs)
+            return cache[key]
+        return memoizer
