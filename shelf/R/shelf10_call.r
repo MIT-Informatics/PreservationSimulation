7b642656cc7ea35ad47196ee054b66db710cf40a
Richard Landau <landau@ricksoft.com>
2016-10-06 17:25:52 -0400

    Make some operational but cosmetic changes to broker output. Change rules to permit really long lifetimes. Add TODOs. Fill out calibration spreadsheet.

--- shelf/R/shelf10_call.r
+++ shelf/R/shelf10_call.r
@@ -1,6 +1,7 @@
 # Edit me and run me.  (Select all, then ctrl-Enter.)
 
-sInputFilename  <- "./GiantOutput_doc50copvarshlf1lifvar_af2500s1tTOTAL_gf30000i100d0m0_seed21-NAfixed-output-00.txt"
-sOutputFilename <- "./q3-af2500s1tTOTAL_gf30000i100d0m0-AnnualAudit_3yrGlitch-analysis-00.txt"
-sTitle <- "Q3 copies=var Glitch random 3yr 100% impact 0hr max, 0hr decay, Annual Audit TOTAL, seed21"
+sInputFilename  <- "./GiantOutput_hl_doc50cop12shlf1lifvar_af0_gf0_seed21_output-00.txt"
+sOutputFilename <- "./hl_AuditNONE_cop12_glitch0_analysis_20161006_00.txt"
+sTitle <- "HL copies=1,2 Glitch=0 Audit NONE seed21"
 source("./ShelfAnalyze-glitchimpact-06.r")
+
---
+++ shelf/R/shelf11_call.r
@@ -0,0 +1,7 @@
+# Edit me and run me.  (In R Studio, select all, then ctrl-Enter.)
+
+sInputFilename  <- "./GiantOutput_hl_doc50cop123shlf1lifvar_af0_gf0_seed21_output-00.txt"
+sOutputFilename <- "./hl_AuditNONE_cop123_glitch0_analysis_20161006_00.txt"
+sTitle <- "HL copies=1,2,3 Glitch=0 Audit NONE seed21"
+source("./ShelfAnalyze-glitchimpact-07.r")
+
