c02a0349322add2793af7b17684e44d4b0960532
Richard Landau <landau@ricksoft.com>
2017-01-11 13:16:46 -0500

    Improve, I think, instruction db, still way too big (4M cases). Add serverdefaultlife to all templates, headers, extraction rules, etc.

--- shelf/dumpparams.py
+++ shelf/dumpparams.py
@@ -47,7 +47,7 @@ def dumpParamsIntoLog():
         (nQuality,nShelfSize) = G.dServerParams[sServer][0]
         lg.logInfo("PARAMS","SERVER server|%s| quality|%d| shelfsize|%d|TB" 
             % (sServer,nQuality,nShelfSize))
-    lg.logInfo("PARAMS","SERVER DefaultHalflife |%s|" 
+    lg.logInfo("PARAMS","SERVER DefaultHalflife|%s|" 
         % (G.fServerDefaultHalflife))
 
     # S h e l f  params
