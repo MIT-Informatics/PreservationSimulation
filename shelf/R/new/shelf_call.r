3c99c38fed908a5a21ade9bc98cd95572ba67756
Richard Landau <landau@ricksoft.com>
2017-03-24 23:54:47 -0400

    Run analyses on 2 and 3 yr shocks.

--- shelf/R/new/shelf_call.r
+++ shelf/R/new/shelf_call.r
@@ -6,10 +6,10 @@
 # - select all (ctrl-A), and 
 # - execute the script (ctrl-Enter).
 
-sInputFilename  <- "../hl/data/GiantOutput_shock5yr_lenvar_00.txt"
-sOutputFilename <- "../hl/tabs/shocktest3_freq5yr_lenvar_seed21_analysis_20170324_00.txt"
-sTitle <- "shocktest3 HL copies=var Audit=1yr Glitch=0 Shock=5yr len=var lifem=var seed21"
-sSubtitle <- "(impact=50,100pct len=var; copies grouped vertically)"
+sInputFilename  <- "../hl/data/GiantOutput_shock2yr_len1yr_00.txt"
+sOutputFilename <- "../hl/tabs/shocktest3_freq2yr_lenvar_seed21_analysis_20170324_00.txt"
+sTitle <- "shocktest3 HL copies=var Audit=1yr Glitch=0 Shock=2yr len=1yr lifem=var seed21"
+sSubtitle <- "(impact=50,100pct len=1yr; copies grouped vertically)"
 sSummarizeFilename <- "./summ-shock-3.r"
 sAnalyzeFilename <- "./ShelfAnalyze.r"
 bTablesOnly <- 0
