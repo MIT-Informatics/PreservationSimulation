831c4f32365481580e7304bb67ed504fbc445ee4
maltman <micah.altman@gmail.com>
2017-07-19 10:17:09 -0400

    analysis code
    aggregation and plotting sample code for paper

---
+++ PaperDraft/ExploreGiantOutputs.R
@@ -0,0 +1,50 @@
+library(tidyverse)
+
+# load & merge all files in directory
+filesList <- grep(pattern = ".*\\.txt$",dir(),value = TRUE)
+sims.df.ls = lapply( filesList, 
+      function(x)read.table("GiantOutput_freq1yr_20170717_doc50_00.txt",
+                            header=TRUE,sep=" ",na.strings="nolinefound")
+) #NOTE MISSING PARAMETER ENTRIES in na.strings
+sims.merged.df <- bind_rows(sims.df.ls)
+
+#set up namesets for dependent, independent, ignored vars
+
+allVarNames <- colnames(sims.merged.df)
+ignoreVarNames <- c("timestamp","seed","walltime","cputime","logfilename","logfilesize","instructionfilename","todaysdatetime","extractorversion","cpuinfo","mongoid")
+resultVarNames <- c("docslost","docsrepairedmajority","docsrepairedminority","lost","deadserversactive","deadserversall","docsokay","auditmajority","auditminority","auditlost","auditrepairs")
+coreResultVarNames<- c("docslost","docsrepairedmajority","docsrepairedminority")
+paramVarNames<- setdiff(setdiff(allVarNames,resultVarNames),ignoreVarNames)
+testVarNames<-c("copies","lifem")
+nonIgnoreVarNames<-setdiff(allVarNames,ignoreVarNames)
+
+# select, group, and aggregate
+sims.selected.df <- sims.merged.df[nonIgnoreVarNames]
+gp_sims.merged<-eval(parse(text=paste("group_by(sims.selected.df,",paste(collapse=",",paramVarNames),")")))
+res<-summarise(gp_sims.merged,mnlossfrac=mean(docslost/docstotal),n=n())
+
+#plot a regression plot between copies, and loss
+library(ggplot2)
+
+gp<-ggplot(res, aes(x=copies, y=mnlossfrac)) +
+  geom_point(shape=1) +    
+  geom_smooth(method = lm)
+plot(gp)
+
+#plot multiples of the plot above, faceted by different values of schockimpact and nshocks
+gp<-ggplot(res, aes(x=copies, y=mnlossfrac)) +
+  geom_point(shape=1) +    
+  geom_smooth(method = lm) + facet_wrap(~shockimpact)
+plot(gp)
+
+gp<-ggplot(res, aes(x=copies, y=mnlossfrac)) +
+  geom_point(shape=1) +    
+  geom_smooth(method = lm) + facet_grid(~shockimpact~nshocks)
+plot(gp)
+
+# pairwise comparison plot of multiple variables -- just for fun
+library(GGally)
+gp<-ggpairs(res,columns=c("copies","mnlossfrac","shockimpact","nshocks"))
+
+
+
