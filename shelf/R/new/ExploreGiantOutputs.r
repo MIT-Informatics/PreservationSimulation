bbad293a8025c05f26579235791f452bd6fa3e46
Richard Landau <landau@ricksoft.com>
2017-09-23 13:59:16 -0400

    Begin (crudely) work on R summaries.

--- shelf/R/new/ExploreGiantOutputs.r
+++ shelf/R/new/ExploreGiantOutputs.r
@@ -4,19 +4,25 @@
 # Trying to understand it, RBLandau 20170920
 
 
-
 library(tidyverse)
 
-# load & merge all files in directory
+# Absurd hack in here to make lifem print reasonably.
+#  Increase the penalty for printing in scientific 
+#  (exponential) format, and limit the number of digits
+#  so that there are no places after the radix point.  
+options("scipen"=100, "digits"=1)
+# End of absurd hack.  
+
+# Load & merge all files in directory
 filesList <- grep(pattern = "^Giant.*\\.txt$", dir(), value = TRUE)
 sims.df.ls = lapply( filesList, 
       function(x)read.table(x,
                             header=TRUE,sep=" ", na.strings="nolinefound")
-) #NOTE MISSING PARAMETER ENTRIES in na.strings
+) # NOTE MISSING PARAMETER ENTRIES in na.strings
 sims.merged.df <- bind_rows(sims.df.ls)
 
-#set up namesets for dependent, independent, ignored vars
-
+# Set up namesets for dependent, independent, ignored vars.
+#  Many of the columns are not relevant to data analysis, only tracking.
 allVarNames <- colnames(sims.merged.df)
 ignoreVarNames <- c("timestamp","seed","walltime","cputime","logfilename",
     "logfilesize","instructionfilename","todaysdatetime","extractorversion",
@@ -30,37 +36,94 @@ paramVarNames<- setdiff(setdiff(allVarNames, resultVarNames), ignoreVarNames)
 testVarNames<-c("copies", "lifem")
 nonIgnoreVarNames<-setdiff(allVarNames, ignoreVarNames)
 
-# select, group, and aggregate
+# Select, group, and aggregate.
 sims.selected.df <- sims.merged.df[nonIgnoreVarNames]
 gp_sims.merged<-eval(parse(text=paste("group_by(sims.selected.df,",
     paste(collapse=",", paramVarNames),")")))
-res<-summarise(gp_sims.merged, mnlossfrac=mean(docslost/docstotal), n=n())
-sres<-res[which(res[["copies"]]!=1),]
+results <- summarise(gp_sims.merged, 
+            mdmlosspct=round(midmean(docslost/docstotal)*100,1), n=n())
+selectedresults <- results[which(results[["copies"]]!=1),]
 
+# P L O T S 
 
-#plot a regression plot between copies, and loss
 library(ggplot2)
 
-gp<-ggplot(sres, aes(x=copies, y=mnlossfrac)) +
+nCopies=3
+gp<-(ggplot(fnSelectCopies(results, nCopies), 
+    aes(x=(lifem), y=(safelog(mdmlosspct))))
+    + geom_point(color="red",size=4)
+    + geom_line(linetype="dashed",color="black",size=1)
+    + scale_x_log10()
+    + annotation_logticks()
+    )
+plot(gp)
+
+if (0) {
+# plot a regression plot between copies, and loss
+gp<-ggplot(selectedresults, aes(x=log10(lifem), y=mdmlosspct)) +
   geom_point(shape=1) +    
   geom_smooth(method = lm)
 plot(gp)
 
 # plot multiples of the plot above, faceted by different values 
 #  of schockimpact and nshocks
-gp<-ggplot(sres, aes(x=copies, y=mnlossfrac)) +
+gp<-ggplot(selectedresults, aes(x=copies, y=mdmlosspct)) +
   geom_point(shape=1) +    
   geom_smooth(method = lm) + facet_wrap(~shockimpact)
 plot(gp)
 
-gp<-ggplot(sres, aes(x=copies, y=mnlossfrac)) +
+gp<-ggplot(selectedresults, aes(x=copies, y=mdmlosspct)) +
   geom_point(shape=1) +    
   geom_smooth(method = lm) + facet_grid(~shockimpact~lifem)
 plot(gp)
+}
 
+if (0) {
 # pairwise comparison plot of multiple variables -- just for fun
 library(GGally)
-ggpairs(res,columns=c("copies", "mnlossfrac", "shockimpact", "lifem"))
+ggpairs(res,columns=c("copies", "mdmlosspct", "shockimpact", "lifem"))
+}
+
+# U T I L I T Y   F U N C T I O N S 
+
+# Better functions for summarizing the loss data.
+# These are more robust than just mean, and more broadly estimated
+#  than median.  
+# midmean is just a trimmed mean of the middle half of the sample.
+# trimean is a less efficient but easier trimmed central measure, 
+#  and was a JWT favorite.  
+trimean <-  function(vect)  
+{   foo <- fivenum(vect); 
+    ans <- 0.5*foo[3] + 0.25*foo[2] + 0.25*foo[4]; 
+    ans 
+}
+midmean <-  function(vect)  
+{   ans <- mean(vect, trim=0.25, na.rm=TRUE); 
+    ans 
+}
+
+# Select only the dataframe rows with N copies.
+fnSelectCopies <- function(dfIn, nCopies)
+{   dfIn[dfIn$copies==nCopies,]
+}
+
+# Safe functions for plotting.
+# Note that safelog(0) returns 0, as needed for sensible axis labeling.  
+safelog <- function(x) {return(log10(x+1))}
+safe    <- function(x) {return(x+0.5)}
+
+
+# Edit history:
+# 20170726  MA  Original version.
+# 20170923  RBL Un-abbreviate variable names for clarity.
+#               Change loss factor to midmean percentage.
+#               Force big integers to print as integers rather than
+#                exponentials.
+#               Nuke all the plotting, which didn't give interesting results
+#                in any case.  Need three dimensions: life, copies, losses.
+#                Concentrate on getting the data in for later processing.  
+# 
+# 
 
 
 
