18fa7163b90a5e8577653c1ad7033f8c3f2faca1
Richard Landau <landau@ricksoft.com>
2017-09-24 19:18:21 -0400

    Make slight improvements to assertions list and r script.

--- shelf/R/new/ExploreGiantOutputs.r
+++ shelf/R/new/ExploreGiantOutputs.r
@@ -13,6 +13,36 @@ library(tidyverse)
 options("scipen"=100, "digits"=1)
 # End of absurd hack.  
 
+# U T I L I T Y   F U N C T I O N S 
+
+# Better functions for summarizing the loss data.
+# These are more robust than just mean, and more broadly estimated
+#  than median.  
+# midmean is just a trimmed mean of the middle half of the sample.
+# trimean is a less efficient but easier trimmed central measure, 
+#  and was a JWT favorite.  
+trimean <-  function(vect)  
+{   foo <- fivenum(vect); 
+    ans <- 0.5*foo[3] + 0.25*foo[2] + 0.25*foo[4]; 
+    ans 
+}
+midmean <-  function(vect)  
+{   ans <- mean(vect, trim=0.25, na.rm=TRUE); 
+    ans 
+}
+
+# Select only the dataframe rows with N copies.
+fnSelectCopies <- function(dfIn, nCopies)
+{   dfIn[dfIn$copies==nCopies,]
+}
+
+# Safe functions for plotting.
+# Note that safelog(0) returns 0, as needed for sensible axis labeling.  
+safelog <- function(x) {return(log10(x+1))}
+safe    <- function(x) {return(x+0.5)}
+
+# L O A D   F I L E S 
+
 # Load & merge all files in directory
 filesList <- grep(pattern = "^Giant.*\\.txt$", dir(), value = TRUE)
 sims.df.ls = lapply( filesList, 
@@ -44,11 +74,18 @@ results <- summarise(gp_sims.merged,
             mdmlosspct=round(midmean(docslost/docstotal)*100,1), n=n())
 selectedresults <- results[which(results[["copies"]]!=1),]
 
+library(reshape2)
+basicdata <- data.frame(cbind(results$copies,results$lifem,results$mdmlosspct))
+colnames(basicdata)<-c("copies","lifem","lossp")
+#basictable <- dcast(basicdata, copies~lifem, FUN=identity(basicdata$lossp))
+basictable <- dcast(results, copies~lifem,  value.var="mdmlosspct")
+#audittable <- basictable <- dcast(results, auditfrequency+copies~lifem,  value.var="mdmlosspct")
+
 # P L O T S 
 
 library(ggplot2)
 
-nCopies=3
+nCopies <- 3
 gp<-(ggplot(fnSelectCopies(results, nCopies), 
     aes(x=(lifem), y=(safelog(mdmlosspct))))
     + geom_point(color="red",size=4)
@@ -84,33 +121,6 @@ library(GGally)
 ggpairs(res,columns=c("copies", "mdmlosspct", "shockimpact", "lifem"))
 }
 
-# U T I L I T Y   F U N C T I O N S 
-
-# Better functions for summarizing the loss data.
-# These are more robust than just mean, and more broadly estimated
-#  than median.  
-# midmean is just a trimmed mean of the middle half of the sample.
-# trimean is a less efficient but easier trimmed central measure, 
-#  and was a JWT favorite.  
-trimean <-  function(vect)  
-{   foo <- fivenum(vect); 
-    ans <- 0.5*foo[3] + 0.25*foo[2] + 0.25*foo[4]; 
-    ans 
-}
-midmean <-  function(vect)  
-{   ans <- mean(vect, trim=0.25, na.rm=TRUE); 
-    ans 
-}
-
-# Select only the dataframe rows with N copies.
-fnSelectCopies <- function(dfIn, nCopies)
-{   dfIn[dfIn$copies==nCopies,]
-}
-
-# Safe functions for plotting.
-# Note that safelog(0) returns 0, as needed for sensible axis labeling.  
-safelog <- function(x) {return(log10(x+1))}
-safe    <- function(x) {return(x+0.5)}
 
 
 # Edit history:
@@ -122,7 +132,7 @@ safe    <- function(x) {return(x+0.5)}
 #               Nuke all the plotting, which didn't give interesting results
 #                in any case.  Need three dimensions: life, copies, losses.
 #                Concentrate on getting the data in for later processing.  
-# 
+# 20170924  RBL Move all functions to the top
 # 
 
 
