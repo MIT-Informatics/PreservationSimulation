49807b3cdbc50fde0817379cad1c1a90311db0bf
Richard Landau <landau@ricksoft.com>
2017-10-06 09:59:31 -0400

    Add baseline-auditannually plot and table script.

--- baseline/auditannually/GetAuditAnnuallyData.r
+++ baseline/auditannually/GetAuditAnnuallyData.r
@@ -1,4 +1,4 @@
-# GetNoAuditData.r
+# GetAnnualAuditData.r
 
 source("./DataUtil.r")
 
@@ -10,17 +10,15 @@ dat.noaudit <- fndfGetAuditData(results)
 tbl.noaudit <- data.frame(with(dat.noaudit, 
             tapply(mdmlosspct, list(copies, lifem), FUN=identity)))
 # Re-form the data into a table for printing.  
-#tbl<-cbind.data.frame(dat.noaudit$copies, tbl.noaudit)
-#tbl<-cbind.data.frame(levels(factor(dat.noaudit$copies)), tbl.noaudit)
-#colnames(tbl)<-c("copies",colnames(tbl.noaudit))
 foo<-(with(dat.noaudit, 
        tapply(mdmlosspct, list(copies, lifem), FUN=identity)))
 foo2<-cbind(as.numeric(levels(factor(results$copies))), foo)
 tbl2<-data.frame(foo2)
 colnames(tbl2)<-c("copies",as.numeric(colnames(foo2[,2:ncol(foo2)])))
+# Pretty-print this table into a separate file, with explanatory headings.  
 #print(tbl2)
 
-# Micah bent dcast to his will; see email.
+# Micah succeeded in bending dcast to his will; see email.
 #library(reshape2)
 #bar.melted<-melt something
 #bar.recast<-dcast something
@@ -28,27 +26,22 @@ colnames(tbl2)<-c("copies",as.numeric(colnames(foo2[,2:ncol(foo2)])))
 # Plot something.
 library(ggplot2)
 
-nCopies <- 1
+# Show lines for 3, 4, 5 copies, with annual total auditing, over wiiiide range.
+nCopies <- 3
 trows <- fnSelectCopies(dat.noaudit, nCopies)
 gp <- ggplot(data=trows,aes(x=lifem,y=safe(mdmlosspct))) 
 gp <- gp + 
         scale_x_log10() + scale_y_log10() +
         annotation_logticks()
+
 gp <- gp + 
         aes(trows, x=(lifem), y=(safe(mdmlosspct))) +
         geom_point(data=trows, 
             color="red", size=5, shape=(48+nCopies)) +
         geom_line(data=trows, 
-            linetype="dashed", color="black", size=1)
+            linetype="dashed", color="blue", size=1)
 
-nCopies <- 2; trows <- fnSelectCopies(dat.noaudit, nCopies)
-gp <- gp + 
-        aes(trows, x=(lifem), y=(safe(mdmlosspct))) +
-        geom_point(data=trows, 
-            color="red", size=5, shape=(48+nCopies)) +
-        geom_line(data=trows, linetype="dashed", color="blue", size=1) 
-
-nCopies <- 3; trows <- fnSelectCopies(dat.noaudit, nCopies)
+nCopies <- 4; trows <- fnSelectCopies(dat.noaudit, nCopies)
 gp <- gp + 
         aes(trows, x=(lifem), y=(safe(mdmlosspct))) +
         geom_point(data=trows, 
@@ -60,25 +53,18 @@ gp <- gp +
         aes(trows, x=(lifem), y=(safe(mdmlosspct))) +
         geom_point(data=trows, 
             color="red", size=5, shape=(48+nCopies)) +
-        geom_line(data=trows, linetype="dashed", color="blue", size=1) 
+        geom_line(data=trows, linetype="dashed", color="black", size=1) 
 
-nCopies <- 9; trows <- fnSelectCopies(dat.noaudit, nCopies)
-gp <- gp + 
-        aes(trows, x=(lifem), y=(safe(mdmlosspct))) +
-        geom_point(data=trows, 
-            color="red", size=5, shape=(48+nCopies)) +
-        geom_line(data=trows, linetype="dashed", color="blue", size=1) 
-
-gp <- gp + ggtitle("Without auditing, we need many copies\nto minimize permanent losses")
+gp <- gp + ggtitle("With annual auditing, we need only a few copies\nto minimize permanent losses over a wide range")
 gp <- gp + xlab("sector half-life (megahours)")
 gp <- gp + ylab("percent permanent document losses")
 gp <- gp + theme(
             axis.text=element_text(size=12),
             axis.title=element_text(size=18),
-            plot.title=element_text(size=22,face="bold"),
+            plot.title=element_text(size=20,face="bold"),
             panel.border = element_rect(color = "black", fill=NA, size=1)
             )
 
 plot(gp)
 
-fnPlotMakeFile(gp, "baseline-noaudit.png")
+fnPlotMakeFile(gp, "baseline-auditannually.png")
