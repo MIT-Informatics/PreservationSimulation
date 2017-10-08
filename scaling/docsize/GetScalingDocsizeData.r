970e72ada20982388999420d7eadb898e234fcd2
Richard Landau <landau@ricksoft.com>
2017-10-08 18:33:53 -0400

    Add prelim graph and table for document size scaling

--- scaling/docsize/GetScalingDocsizeData.r
+++ scaling/docsize/GetScalingDocsizeData.r
@@ -6,44 +6,51 @@ source("./DataUtil.r")
 rm(results)
 results <- fndfGetGiantData("./")
 # Get fewer columns to work with, easier to see.
-dat.auditannually <- fndfGetAuditData(results)
+dat.docsize <- fndfGetDocsizeData(results)
 
 # T A B U L A T E   D A T A 
 # Tabulate columns by copies and sector lifetime.
-tbl.auditannually <- data.frame(with(dat.auditannually, 
-            tapply(mdmlosspct, list(copies, lifem), FUN=identity)))
+tbl.docsize <- data.frame(with(dat.docsize, 
+            tapply(mdmlosspct, list(docsize, lifem), FUN=identity)))
 # Re-form the data into a table for printing.  
-foo<-(with(dat.auditannually, 
-       tapply(mdmlosspct, list(copies, lifem), FUN=identity)))
-foo2<-cbind(as.numeric(levels(factor(results$copies))), foo)
+foo<-(with(dat.docsize, 
+       tapply(mdmlosspct, list(docsize, lifem), FUN=identity)))
+foo2<-cbind(as.numeric(levels(factor(results$docsize))), foo)
 tbl2<-data.frame(foo2)
-colnames(tbl2)<-c("copies",as.numeric(colnames(foo2[,2:ncol(foo2)])))
+colnames(tbl2)<-c("docsize",as.numeric(colnames(foo2[,2:ncol(foo2)])))
+
 # Pretty-print this to a file with explanatory headings.
-sOutputFilename <- "./Data_AuditAnnually.txt"
-sTitle <- "Document losses with multiple copies and annual total auditing"
+sOutputFilename <- "./Data_ScalingDocsize.txt"
+sTitle <- "Document losses with single copy and varying document size"
 sSubtitle <- "Table of percentage loss of collection tabulated\n" %+% 
-    "by number of copies and sector half-lifetime"
+    "by document size (MB) and sector half-lifetime" %+% "\n\n" %+%
+    "Note how document size and sector life scale together" %+% "\n\n\n" %+%
+    "               sector half-life (megahours)"
 sink(sOutputFilename)
 cat("MIT Preservation Simulation Project", "\n\n")
 cat(sOutputFilename, format(Sys.time(),"%Y%m%d_%H%M%S%Z"), "\n\n")
 cat(sTitle, "\n\n")
 cat(sSubtitle, "\n\n")
 cat("\n")
-print(tbl2)
-sink()
-
 # Micah succeeded in bending dcast to his will, thanks; see email.
 library(reshape2)
-bar.small <- dat.auditannually[,c("copies", "lifem", "mdmlosspct")]
-bar.melted <- melt(bar.small, id=c("copies", "lifem"))
-bar.recast <- dcast(bar.melted, copies~lifem)
+bar.small <- dat.docsize[,c("lifem", "docsize", "mdmlosspct")]
+bar.melted <- melt(bar.small, id=c("docsize", "lifem"))
+bar.recast <- dcast(bar.melted, docsize~lifem)
+print(bar.recast)
+sink()
 
+
+
+
+
+
+#if(0){
 # P L O T   D A T A 
 library(ggplot2)
 
 # Show lines for 3, 4, 5 copies, with annual total auditing, over wiiiide range.
-nCopies <- 3
-trows <- fnSelectCopies(dat.auditannually, nCopies)
+nDocsize <- 5; trows <- dat.docsize[dat.docsize$docsize==nDocsize,]
 gp <- ggplot(data=trows,aes(x=lifem,y=safe(mdmlosspct))) 
 gp <- gp + 
         scale_x_log10() + scale_y_log10() +
@@ -52,36 +59,49 @@ gp <- gp +
 gp <- gp + 
         aes(trows, x=(lifem), y=(safe(mdmlosspct))) +
         geom_point(data=trows, 
-            color="red", size=5, shape=(48+nCopies)) +
+            color="red", size=5, shape=("5")) +
         geom_line(data=trows, 
             linetype="dashed", color="blue", size=1)
 
-nCopies <- 4; trows <- fnSelectCopies(dat.auditannually, nCopies)
+nDocsize <- 50; trows <- dat.docsize[dat.docsize$docsize==nDocsize,]
 gp <- gp + 
         aes(trows, x=(lifem), y=(safe(mdmlosspct))) +
         geom_point(data=trows, 
-            color="red", size=5, shape=(48+nCopies)) +
+            color="red", size=5, shape=("B")) +
         geom_line(data=trows, linetype="dashed", color="blue", size=1) 
 
-nCopies <- 5; trows <- fnSelectCopies(dat.auditannually, nCopies)
+nDocsize <- 500; trows <- dat.docsize[dat.docsize$docsize==nDocsize,]
 gp <- gp + 
         aes(trows, x=(lifem), y=(safe(mdmlosspct))) +
         geom_point(data=trows, 
-            color="red", size=5, shape=(48+nCopies)) +
+            color="red", size=5, shape=("C")) +
         geom_line(data=trows, linetype="dashed", color="black", size=1) 
 
-gp <- gp + ggtitle("With annual auditing, we need only a few copies\nto minimize permanent losses over a wide range")
+nDocsize <- 5000; trows <- dat.docsize[dat.docsize$docsize==nDocsize,]
+gp <- gp + 
+        aes(trows, x=(lifem), y=(safe(mdmlosspct))) +
+        geom_point(data=trows, 
+            color="red", size=5, shape=("D")) +
+        geom_line(data=trows, linetype="dashed", color="black", size=1) 
+
+gp <- gp + ggtitle("Larger documents are larger targets to random errors\n" %+%
+                "and therefore are lost more frequently.\n\n" %+%
+                "Losses for collection with copies=1")
 gp <- gp + xlab("sector half-life (megahours)")
 gp <- gp + ylab("percent permanent document losses")
 gp <- gp + theme(
             axis.text=element_text(size=12),
             axis.title=element_text(size=18),
-            plot.title=element_text(size=20,face="bold"),
+            plot.title=element_text(size=18,face="bold"),
             panel.border = element_rect(color = "black", fill=NA, size=1)
             )
 
 plot(gp)
-fnPlotMakeFile(gp, "baseline-auditannually.png")
+fnPlotMakeFile(gp, "baseline-scalingdocsize.png")
+#}
+
+
+
 
 # Unwind any remaining sink()s to close output files.  
 while (sink.number() > 0) {sink()}
