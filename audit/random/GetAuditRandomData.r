c583412208e33ffc576386d7f92729a01a8495ee
Richard Landau <landau@ricksoft.com>
2017-11-13 23:56:22 -0500

    Initial pics for random auditing; still serious questions about labels and limits.

--- audit/random/GetAuditRandomData.r
+++ audit/random/GetAuditRandomData.r
@@ -7,12 +7,33 @@ source("./PlotUtil.r")
 rm(results)
 results <- fndfGetGiantData("./")
 # Get fewer columns to work with, easier to see.
-dat.auditrandom <- fndfGetAuditData(results)
+dat.auditrandom <- as.data.frame(fndfGetAuditData(results))
 
-# T A B U L A T E   D A T A 
-# Tabulate columns by copies and sector lifetime.
-tbl.auditrandom <- data.frame(with(dat.auditrandom, 
-            tapply(mdmlosspct, list(copies, lifem), FUN=identity)))
+# S U B S E T S 
+dat.auditrandom <- dat.auditrandom[dat.auditrandom$lifem<=100,]
+dat.auditbaseline <- dat.auditrandom[dat.auditrandom$auditsegments==1 
+                                    & dat.auditrandom$lifem>=3,]
+dat.auditrandom.monthly <- dat.auditrandom[dat.auditrandom$auditsegments==10,]
+dat.auditrandom.quarterly <- dat.auditrandom[dat.auditrandom$auditsegments==4,]
+dat.auditrandom.semiannually <- dat.auditrandom[dat.auditrandom$auditsegments==2,]
+
+source("RandomTables.r")
+
+# T A B U L A T E   D A T A  A S   T E X T 
+
+# M O N T H L Y 
+sOutputFilename <- "./Data_AuditRandom_Monthly.txt"
+sTitle <- "Document losses with multiple copies and MONTHLY random auditing"
+sSubtitle <- "Table of percentage loss of collection tabulated\n" %+% 
+    "by number of copies and sector half-lifetime"
+dat.tmp <- dat.auditrandom.monthly
+tbl.tmp <- fnPrintTableRandom(dat.tmp, sOutputFilename, sTitle, sSubtitle
+)
+
+# IF THAT WORKS, DO SOME MORE.
+
+
+
 # Re-form the data into a table for printing.  
 foo<-(with(dat.auditrandom, 
        tapply(mdmlosspct, list(copies, lifem), FUN=identity)))
@@ -20,10 +41,6 @@ foo2<-cbind(as.numeric(levels(factor(results$copies))), foo)
 tbl2<-data.frame(foo2)
 colnames(tbl2)<-c("copies",as.numeric(colnames(foo2[,2:ncol(foo2)])))
 # Pretty-print this to a file with explanatory headings.
-sOutputFilename <- "./Data_AuditRandom.txt"
-sTitle <- "Document losses with multiple copies and levels of random auditing"
-sSubtitle <- "Table of percentage loss of collection tabulated\n" %+% 
-    "by number of copies and sector half-lifetime"
 sink(sOutputFilename)
 cat("MIT Preservation Simulation Project", "\n\n")
 cat(sOutputFilename, format(Sys.time(),"%Y%m%d_%H%M%S%Z"), "\n\n")
@@ -42,62 +59,55 @@ bar.recast <- dcast(bar.melted, copies~lifem)
 # P L O T   D A T A 
 library(ggplot2)
 
-# TMP
-dat.auditrandom <- dat.auditrandom[dat.auditrandom$lifem<=100,]
-dat.auditbaseline <- dat.auditrandom[dat.auditrandom$auditsegments==1,]
-dat.auditrandom.monthly <- dat.auditrandom[dat.auditrandom$auditsegments==10,]
-dat.auditrandom.quarterly <- dat.auditrandom[dat.auditrandom$auditsegments==4,]
-dat.auditrandom.semiannually <- dat.auditrandom[dat.auditrandom$auditsegments==2,]
-
 # Compare random qtly and monthly against total annual.
 
+source("RandomPlots.r")
 
+# R A N D O M   V S   B A S E L I N E 
+
+# Show lines for 3 copies, with random segmented auditing, over wiiiide range.
+nCopies <- 3
+gp <- fnPlotRandomMonthlyVsBaseline(nCopies)
+plot(gp)
+fnPlotMakeFile(gp, "baseline-auditrandomvsbaseline-c3.png")
+
+# Show lines for 4 copies, with random segmented auditing, over wiiiide range.
+nCopies <- 4
+gp <- fnPlotRandomMonthlyVsBaseline(nCopies)
+plot(gp)
+fnPlotMakeFile(gp, "baseline-auditrandomvsbaseline-c4.png")
+
+# Show lines for 5 copies, with random segmented auditing, over wiiiide range.
+nCopies <- 5
+gp <- fnPlotRandomMonthlyVsBaseline(nCopies)
+plot(gp)
+fnPlotMakeFile(gp, "baseline-auditrandomvsbaseline-c5.png")
+
+
+
+
+# M O R E   R A N D O M   S E G M E N T S 
 
 # Compare random semiannual, quarterly, and monthly.
 
-
-
-# Show lines for 3, 4, 5 copies, with random segmented auditing, over wiiiide range.
+# Show lines for 3 copies, with random segmented auditing, over wiiiide range.
 nCopies <- 3
-trows <- fnSelectCopies(dat.auditrandom, nCopies)
-gp <- ggplot(data=trows,aes(x=lifem,y=safe(mdmlosspct))) 
-gp <- gp + 
-        scale_x_log10() + scale_y_log10() +
-        annotation_logticks()
-
-gp <- gp + 
-        aes(trows, x=(lifem), y=(safe(mdmlosspct))) +
-        geom_point(data=trows, 
-            color="red", size=5, shape=(48+nCopies)) +
-        geom_line(data=trows, 
-            linetype="dashed", color="blue", size=1)
-
-nCopies <- 4; trows <- fnSelectCopies(dat.auditrandom, nCopies)
-gp <- gp + 
-        aes(trows, x=(lifem), y=(safe(mdmlosspct))) +
-        geom_point(data=trows, 
-            color="red", size=5, shape=(48+nCopies)) +
-        geom_line(data=trows, linetype="dashed", color="blue", size=1) 
-
-nCopies <- 5; trows <- fnSelectCopies(dat.auditrandom, nCopies)
-gp <- gp + 
-        aes(trows, x=(lifem), y=(safe(mdmlosspct))) +
-        geom_point(data=trows, 
-            color="red", size=5, shape=(48+nCopies)) +
-        geom_line(data=trows, linetype="dashed", color="black", size=1) 
-
-gp <- gp + ggtitle("With random auditing, sampling WITH replacement,\nsome docs are missed entirely, increasing losses")
-gp <- gp + xlab("sector half-life (megahours)")
-gp <- gp + ylab("percent permanent document losses")
-gp <- gp + theme(
-            axis.text=element_text(size=12),
-            axis.title=element_text(size=18),
-            plot.title=element_text(size=20,face="bold"),
-            panel.border = element_rect(color = "black", fill=NA, size=1)
-            )
-
+gp <- fnPlotRandomVariousSegments(nCopies)
 plot(gp)
-fnPlotMakeFile(gp, "baseline-auditrandom.png")
+fnPlotMakeFile(gp, "baseline-auditrandomvarioussegments-c3.png")
+
+# Show lines for 4 copies, with random segmented auditing, over wiiiide range.
+nCopies <- 4
+gp <- fnPlotRandomVariousSegments(nCopies)
+plot(gp)
+fnPlotMakeFile(gp, "baseline-auditrandomvarioussegments-c4.png")
+
+# Show lines for 5 copies, with random segmented auditing, over wiiiide range.
+nCopies <- 5
+gp <- fnPlotRandomVariousSegments(nCopies)
+plot(gp)
+fnPlotMakeFile(gp, "baseline-auditrandomvarioussegments-c5.png")
+
 
 # Unwind any remaining sink()s to close output files.  
 while (sink.number() > 0) {sink()}
---
+++ audit/random/RandomPlots.r
@@ -0,0 +1,124 @@
+# RandomPlots.r
+#               RBLandau 20171112
+
+# f n P l o t R a n d o m M o n t h l y V s B a s e l i n e 
+fnPlotRandomMonthlyVsBaseline <- function(nCopies){
+
+    #  M O N T H L Y  ( M )
+    trows <- fnSelectCopies(dat.auditrandom.monthly, nCopies)
+    gp <- ggplot(data=trows,aes(x=lifem,y=safe(mdmlosspct))) 
+    gp <- gp +  
+            scale_x_continuous() + scale_y_continuous() + 
+            scale_x_log10("sector half-life (megahours) A") + 
+            scale_y_log10("% permanent document losses B") +
+            ylim(0,5) + 
+            annotation_logticks()
+
+    gp <- gp + 
+            aes(trows, x=(lifem), y=(safe(mdmlosspct))) +
+            geom_point(data=trows, 
+                color="red", size=5, shape='M') +
+            geom_line(data=trows, 
+                linetype="dashed", color="blue", size=1)
+
+    # B A S E L I N E   T O T A L   A U D I T  ( number ) 
+    trows <- fnSelectCopies(dat.auditbaseline, nCopies)
+    gp <- gp + aes(data=trows,aes(x=lifem,y=safe(mdmlosspct))) 
+    gp <- gp + 
+            scale_x_continuous() + scale_y_continuous() + 
+            scale_x_log10("sector half-life (megahours) C") + 
+            scale_y_log10("% permanent document losses D") + 
+            ylim(0,5) + 
+            annotation_logticks()
+
+    gp <- gp + 
+            aes(trows, x=(lifem), y=(safe(mdmlosspct))) +
+            geom_point(data=trows, 
+                color="red", size=5, shape=(48+nCopies)) +
+            geom_line(data=trows, 
+                linetype="dashed", color="blue", size=1)
+
+    gp <- gp + ggtitle(sprintf("(copies = %s)",nCopies) 
+                        %+% " With random auditing,"
+                        %+% "\nsampling WITH replacement," 
+                        %+% "\nsome docs are missed entirely, increasing losses")
+    gp <- gp + xlab("sector half-life (megahours) E")
+    gp <- gp + ylab("% permanent document losses F")
+    gp <- gp + theme(
+                axis.text=element_text(size=12),
+                axis.title=element_text(size=18),
+                plot.title=element_text(size=20,face="bold"),
+                panel.border = element_rect(color = "black", fill=NA, size=1)
+                )
+
+    return(gp)
+}
+
+# f n P l o t R a n d o m V a r i o u s S e g m e n t s 
+fnPlotRandomVariousSegments <- function(nCopies){
+
+    #  M O N T H L Y  ( M )
+    trows <- fnSelectCopies(dat.auditrandom.monthly, nCopies)
+    gp <- ggplot(data=trows,aes(x=lifem,y=safe(mdmlosspct))) 
+    gp <- gp + 
+            scale_x_log10("sector half-life (megahours) G") + 
+            scale_y_log10("% permanent document losses H") +
+            ylim(0,5) + 
+            annotation_logticks()
+
+    gp <- gp + 
+            aes(trows, x=(lifem), y=(safe(mdmlosspct))) +
+            geom_point(data=trows, 
+                color="red", size=5, shape='M') +
+            geom_line(data=trows, 
+                linetype="dashed", color="blue", size=1)
+
+    # Q U A R T E R L Y  ( Q ) 
+    trows <- fnSelectCopies(dat.auditrandom.quarterly, nCopies)
+    gp <- gp + aes(data=trows,aes(x=lifem,y=safe(mdmlosspct))) 
+    gp <- gp + 
+            scale_x_log10("sector half-life (megahours) I") + 
+            scale_y_log10("% permanent document losses J") +
+            annotation_logticks()
+
+    gp <- gp + 
+            aes(trows, x=(lifem), y=(safe(mdmlosspct))) +
+            geom_point(data=trows, 
+                color="red", size=5, shape='Q') +
+            geom_line(data=trows, 
+                linetype="dashed", color="blue", size=1)
+
+    # S E M I A N N U A L L Y  ( S ) 
+    trows <- fnSelectCopies(dat.auditrandom.semiannually, nCopies)
+    gp <- gp + aes(data=trows,aes(x=lifem,y=safe(mdmlosspct))) 
+    gp <- gp + 
+            scale_x_log10("sector half-life (megahours) K") + 
+            scale_y_log10("% permanent document losses L") +
+            annotation_logticks()
+
+    gp <- gp + 
+            aes(trows, x=(lifem), y=(safe(mdmlosspct))) +
+            geom_point(data=trows, 
+                color="red", size=5, shape='S') +
+            geom_line(data=trows, 
+                linetype="dashed", color="blue", size=1)
+
+    gp <- gp + ggtitle(sprintf("(copies = %s)",nCopies) 
+                        %+% " With random auditing,"
+                        %+% "\nsampling WITH replacement," 
+                        %+% "\nmore frequent small segments are actually worse")
+    gp <- gp + xlab("sector half-life (megahours)")
+    gp <- gp + ylab("% permanent document losses")
+    gp <- gp + theme(
+                axis.text=element_text(size=12),
+                axis.title=element_text(size=18),
+                plot.title=element_text(size=20,face="bold"),
+                panel.border = element_rect(color = "black", fill=NA, size=1)
+                )
+
+    return(gp)
+}
+
+
+
+#END
