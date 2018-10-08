2fc1ca1fee71381b1eb96693f01d6f00d4991003
Richard Landau <landau@ricksoft.com>
2018-10-08 15:44:04 -0400

    Add hi-res (samples=1000) data for span=2 plots. Add quarterly segmented auditing for span=2 as an improved strategy for high shock rates.

--- pictures/SHOCKSPAN2/RUNME.r
+++ pictures/SHOCKSPAN2/RUNME.r
@@ -2,6 +2,7 @@
 # setwd("C:/cygwin64/home/landau/working/PreservationSimulation/pictures/SHOCKSPAN2")
 debugprint<-0
 source("./GetShockSpan2Server3Data.r")
+source("./GetShockSpan2Server4Data.r")
 #source("./GetShockSpan1Server2Data.r")
 #source("./GetShockSpan1Server3Data.r")
 #source("./GetShockSpan1Server4Data.r")
