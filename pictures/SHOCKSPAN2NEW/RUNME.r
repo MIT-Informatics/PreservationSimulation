fcda826f0de6e322f5342dfa25ccc7edd37bc953
rblandau <landau@ricksoft.com>
2018-12-20 19:22:11 -0500

    Add data, r, and pix for shockspan2.  Results not encouraging, if you're a librarian.

--- pictures/SHOCKSPAN2NEW/RUNME.r
+++ pictures/SHOCKSPAN2NEW/RUNME.r
@@ -1,9 +1,8 @@
 # where to run?
-# setwd("C:/cygwin64/home/landau/working/PreservationSimulation/pictures/SHOCKSPAN1NEW")
+# setwd("C:/cygwin64/home/landau/working/PreservationSimulation/pictures/SHOCKSPAN2NEW")
 debugprint<-0
-#source("./GetShockSpan1Server1Data.r")
-#source("./GetShockSpan1Server2Data.r")
-source("./GetShockSpan1Server3Seg1Data.r")
-source("./GetShockSpan1Server3Seg4Data.r")
-#source("./GetShockSpan1Server4Data.r")
-#source("./GetShockSpan1Server1234Data.r")
+source("./GetShockSpan2Server3Seg4Data.r")
+source("./GetShockSpan2Server4Seg4Data.r")
+source("./GetShockSpan2Server5Seg4Data.r")
+source("./GetShockSpan2Server8Seg4Data.r")
+#source("./GetShockSpan2Server1234Seg4Data.r")
