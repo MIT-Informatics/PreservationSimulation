b22dcef8c0630cd968a024ae228aa07b91563c29
rblandau <landau@ricksoft.com>
2019-02-12 12:06:00 -0500

    Re-simplify R code for SHOCKSPAN2, too.

--- pictures/SHOCKSPAN2NEW/GetShockSpan2Server4Seg10Data.r
+++ pictures/SHOCKSPAN2NEW/GetShockSpan2Server4Seg10Data.r
@@ -9,41 +9,10 @@ nShockspan <- 2
 nServerDefaultLife <- 4
 nSegments <- 10
 
-# S T A N D A R D   S H O C K   F O R M A T T I N G 
-sPlotFile <- (  sprintf("shockspan%d",nShockspan)
-            %+% sprintf("server%d",nServerDefaultLife)
-            %+% sprintf("seg%d",nSegments)
-            %+% ".png"
-            )
-fnGroupBy <- function(dfIn) {group_by(dfIn, copies, lifem
-                                    , serverdefaultlife
-                                    , shockfreq, shockspan, shockimpact
-                                    , auditfrequency, auditsegments
-                                    )}
-fnSubset <- function(dfIn)  {subset(dfIn, 
-                                      serverdefaultlife==nServerDefaultLife * 10000
-                                    & shockspan==nShockspan
-                                    & copies>=5
-                                    & auditfrequency==10000
-                                    & auditsegments==nSegments
-                                    )}
-want.varnames <- c("copies","lifem","lost","docstotal","serverdefaultlife"
-                ,"shockfreq","shockimpact","shockspan","shockmaxlife"
-                ,"auditfrequency","audittype","auditsegments"
-                ,"deadserversactive","deadserversall")
-fnNarrow <- function(dfIn)  {dfIn[want.varnames]}  
 
-# G E T   D A T A  
-# Get the data into the right form for these plots.
-alldat.df <- fndfGetGiantDataRaw("")
-allnewdat <- alldat.df %>% fnNarrow() %>% fnGroupBy() %>% 
-summarize(losspct=round(mean(lost/docstotal)*100.0, 2), n=n()) 
-newdat <- fnSubset(allnewdat)
-trows <- newdat
-
+# S T A N D A R D   P R O C E S S I N G   F O R   S H O C K S 
+source("../common/shockformat.r")
 source("../common/shocktitles.r")
-# ************ Also change summarize function and ggplot(color=...).
-# E N D   O F   S T A N D A R D   F O R M A T T I N G 
 
 
 # P L O T   D A T A 
