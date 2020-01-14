e58661fa5a74d073d6c0806ccb05c122c8be94d7
rblandau <landau@ricksoft.com>
2020-01-13 19:06:00 -0500

    Delete moldy, old files that would confuse users. Move some image files around to better homes. Add list of doc files that need to be edited before release.

--- shelf/R/OLDOLD/shelf10_call.r
+++
@@ -1,7 +0,0 @@
-# Edit me and run me.  (Select all, then ctrl-Enter.)
-
-sInputFilename  <- "./GiantOutput_hl_doc50cop12shlf1lifvar_af0_gf0_seed21_output-00.txt"
-sOutputFilename <- "./hl_AuditNONE_cop12_glitch0_analysis_20161006_00.txt"
-sTitle <- "HL copies=1,2 Glitch=0 Audit NONE seed21"
-source("./ShelfAnalyze-glitchimpact-06.r")
-
--- shelf/R/OLDOLD/shelf9_call.r
+++
@@ -1,6 +0,0 @@
-# Edit me and run me.  (Select all, then ctrl-Enter.)
-
-sInputFilename  <- "./GiantOutput_doc50copvarshlf1lifvar_af2500s1tTOTAL_gf30000i100d0m0_seed21-NAfixed-output-00.txt"
-sOutputFilename <- "./q3-af2500s1tTOTAL_gf30000i100d0m0-AnnualAudit_3yrGlitch-analysis-00.txt"
-sTitle <- "Q3 copies=var Glitch random 3yr 100% impact 0hr max, 0hr decay, Annual Audit TOTAL, seed21"
-source("./ShelfAnalyze-glitchimpact-05.r")
