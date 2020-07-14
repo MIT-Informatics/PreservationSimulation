327083ecb74632c5664542557545906eca0f2282
rblandau <landau@ricksoft.com>
2020-07-14 13:32:33 -0400

    Change from pool to individual Processes.  Works.

--- shelf/maybenew/t15main.py
+++ shelf/maybenew/t15main.py
@@ -5,10 +5,8 @@
 '''
 argv[1] = total number of cases to run; default 20.
 argv[2] = max number to run simultaneously; default=number of CPUs available.
-argv[3] = sleep interval in loop waiting for open process slot; 
-            default 50 (msec).
-argv[4] = max number of times to sleep waiting for an open process slot; 
-            default = 100.
+argv[3] = overbooking percentage, default 0.  
+argv[4] = time between sending job instructions, in msec, default 100.          
 '''
 
 import multiprocessing as mp
@@ -18,124 +16,179 @@ import sys
 import collections
 import re
 from NewTraceFac import NTRC, ntrace, ntracef
-from t15docase import fntDoOneCase, tLinesOut, tInstruction
+from t15docase import tLinesOut, tInstruction, doManyJobs, defaultReceiveOutput
 
 
-def printnow(sWhen):
-    print(sWhen, time.process_time_ns(), time.perf_counter(), time.time())
-    pass
+# Global data, read-only, or at most write-once.
+class CG(object):
+    qJobs = None
+    qOutput = None
+    lockPrint = None
+    procOutput = None
     
-def before():
-    return time.time()
+    # =================== test instruction stream =====================
+    # A few lines just to see if the process works.  
+    lLines = ["date +%Y%m%d_%H%M%S.%3N"
+            , "# comment 1"
+            , "pwd"
+            , "ls | head -3"
+            , ""
+            , "# comment 2"
+            , "cat /proc/version"
+            , "#ps | grep python | grep -v grep | head -3"
+            , "date +%Y%m%d_%H%M%S.%3N"
+            ]
 
-def after():
-    return time.time()
+    # Even fewer lines just to see if the process works.  
+    lLines = ["date +%Y%m%d_%H%M%S.%3N"
+            , "# comment 1"
+            , "pwd"
+            ]
 
-def printdiff(beforetime, aftertime):
-    pass
-    diff = aftertime - beforetime
-    print("diff time = %.3f" % diff)
+    # Poison message to cause workers to exit.
+    tEndMsg = tInstruction(cmdlist="", qoutput=qOutput, logdir='', logname='')
+    tEndOutMsg = tLinesOut(listoflists='', procname='')
 
+# =======================================
+# f n C r e a t e W o r k e r P r o c e s s e s 
+@ntrace
+def fnCreateWorkerProcesses(mynHowMany):
+    ''' Create worker pool and start them all. '''
+    lProcs = []
+    for _ in range(g.nParallel):
+        proc = mp.Process(target=doManyJobs, args=(g.qJobs,))
+        lProcs.append(proc)
+        proc.start()
+    return lProcs
 
-# f n b D o N o t I g n o r e L i n e 
-@ntracef("UTIL", level=5)
-def fnbDoNotIgnoreLine(mysLine):
+# =======================================
+# f n S e n d I n s t r u c t i o n s 
+@ntrace
+def fnSendInstructions(mygInstructionSource, mynHowMany, mynPoliteTimerMsec):
+    ''' Send all instructions, or a chunk of them, to the jobs queue.
+        HowMany == 0 implies send all, exhaust the generator.
     '''
-    True if not a comment or blank line.
-    '''
-    # Ignore comment and blank lines, but take all others.
-    return (not re.match("^\s*#", mysLine)) and (not re.match("^\s*$", mysLine))
+    nCountInst = 0
+
+    def fnSendOneInstruction(mytInst, mynPoliteTime):
+        ''' Send just one instruction, and wait a polite amount of time.'''
+        g.qJobs.put(mytInst)
+        NTRC.ntrace(5, "proc SendInstructions sent|%s|" % (repr(tInst)))
+        time.sleep(mynPoliteTime/1000.0)
+        
+    if mynHowMany:
+        for _ in range(mynHowMany):
+            tInst = next(mygInstructionSource)
+            fnSendOneInstruction(tInst, mynPoliteTimerMsec)
+            nCountInst += 1
+    else:
+        for tInst in mygInstructionSource:
+            fnSendOneInstruction(tInst, mynPoliteTimerMsec)
+            nCountInst += 1
+    return nCountInst
 
 
-# A few lines just to see if the process works.  
-lLines = ["date +%Y%m%d_%H%M%S.%3N"
-        , "# comment 1"
-        , "pwd"
-        , "ls | head -3"
-        , ""
-        , "# comment 2"
-        , "cat /proc/version"
-        , "#ps | grep python | grep -v grep | head -3"
-        , "date +%Y%m%d_%H%M%S.%3N"
-        ]
+# =======================================
+# f n S e n d A l l E n d M e s s a g e s 
+# Send out suicide messages.
+def fnSendAllEndMessages():
+    for _ in range(g.nParallel):
+        g.qJobs.put(g.tEndMsg)
+    
 
+# =======================================
+# f n C r e a t e O u t p u t R e c e i v e r 
+@ntrace
+def fnCreateOutputReceiver(myqOutput, myLockPrint, myfReceiver=None):
+    ''' Make one more async process to receive job outputs. '''
+    if not myfReceiver:
+        # Take default if none specified.
+        procRcvrToday = defaultReceiveOutput
+    else:
+        procRcvrToday = myfReceiver
 
+    procReceiver = mp.Process(target=procRcvrToday, 
+                        args=(myqOutput, myLockPrint))
+    procReceiver.start()
+    NTRC.ntrace(3, "proc rcvr|%s| started on q|%s|" % (procRcvrToday, g.qOutput))
+    return procReceiver
+
+# ===================== main ========================
 @ntrace
 def main():
 
+    # =======================================
+    # Get CLI input args if any.
     nArgs = len(sys.argv)
     nCases = 20 
     if nArgs > 1: nCases = int(sys.argv[1]) if sys.argv[1] else nCases
-    nParallel = mp.cpu_count()
-    if nArgs > 2: nParallel = int(sys.argv[2]) if sys.argv[2] else nParallel
-    nWaitMsec = 50
-    if nArgs > 3: nWaitMsec = int(sys.argv[3]) if sys.argv[3] else nWaitMsec
-    nWaitHowMany = 100
-    if nArgs > 4: nWaitHowMany = int(sys.argv[4]) if sys.argv[4] else nWaitHowMany
+    g.nParallel = mp.cpu_count()
+    if nArgs > 2: g.nParallel = int(sys.argv[2]) if sys.argv[2] else g.nParallel
+    nOverbookPct = 0
+    if nArgs > 3: nOverbookPct = int(sys.argv[3]) if sys.argv[3] else nOverbookPct
+    nPoliteTimerMsec = 100
+    if nArgs > 4: nPoliteTimerMsec = int(sys.argv[4]) if sys.argv[4] else nPoliteTimerMsec
     NTRC.ntrace(0, "proc params ncases|%s| nparallel|%s| "
-                    "nwaitmsec|%s| nwaitmany|%s|" 
-                % (nCases, nParallel, nWaitMsec, nWaitHowMany))
+                    "overbookpct|%s| politetime|%s|" 
+                % (nCases, g.nParallel, nOverbookPct, nPoliteTimerMsec))
 
-    # Need a Multiprocessing Manager to own the output queue.
-    multiMgr = mp.Manager()
-    qOut = multiMgr.Queue()
+    # =======================================
+    # Need a Multiprocessing Manager to own the output queue.  (Do we?)
+    mpmgr = mp.Manager()
+    g.qJobs = mp.Queue()
+    g.qOutput = mpmgr.Queue()
+    # And a lock for safety of stdout printing.
+    g.lockPrint = mp.Lock()
     
-    # Ah, this won't work because pool.map cannot pickle a generator, oops.  
-    # This means we have to render the entire instruction sequence into a list,
-    #  which is sort of okay for a few things, but not for thousands at a time.
-    #  Grumble.  Find another way without overly complicating the code.  
-    # Never mind.
-    def gentInstruction(myllsInstructions):
-        ''' cheap generator to paste argument list together for callee'''
-        for (nCaseNumber, lsInstr) in enumerate(myllsInstructions):
-            tPacket = tInstruction(cmdlist=lsInstr
-                    , qoutput=qOut
-                    , logdir="."
-                    , logname=f't15foo{(nCaseNumber+1):03d}.log')
-            yield tPacket
-    # End of nevermind.
-    
-    # Create worker pool.
-    pool = mp.Pool(processes=nParallel)
-    
+    # =======================================
+    # Maybe increase worker pool by some overbooking amount.
+    g.nParallel *= int(1.0 + (nOverbookPct / 100.0))
+    lprocWorkers = fnCreateWorkerProcesses(g.nParallel)
+
+    # =======================================
+    # Dumb testing version, same instructions many times.
     # Put all the instructions into a list of tInstruction tuples.
-    llsInstructions = [lLines for _ in range(nCases)]  # Dummy repetitive list.
-    ltInstructions = [tInstruction(cmdlist=lsInstr
-                        , qoutput=qOut
+    llsInstructions = [g.lLines for _ in range(nCases)]  # Dummy repetitive list.
+    # Generator for the instruction stream, merely reads the list.  
+    gtInstructions = (tInstruction(cmdlist=lsInstr
+                        , qoutput=g.qOutput
                         , logdir="./tmp"
                         , logname=f't15foo{(nCaseNumber+1):03d}.log')
-                        for (nCaseNumber, lsInstr) in enumerate(llsInstructions)]
+                        for (nCaseNumber, lsInstr) in enumerate(llsInstructions)
+                        )
 
-    bef = before()
-    printnow('map before')
-    results = pool.map(fntDoOneCase, (ltInstructions))
-    printnow('map after ')
-    pool.close()
-    pool.join()
-    aft = after()
-    printdiff(bef, aft)
-    NTRC.ntrace(5, "map giant results = |%s|" % (results))
+    # =======================================
+    # Make one more async process to receive job outputs.
+    g.procOutput = fnCreateOutputReceiver(g.qOutput, g.lockPrint)
 
-    # Get all output from the multiprocessing queue.
-    # For long runs, need to find a way to get this gradually, too, 
-    #  rather than waiting for the thousands of jobs to finish
-    #  and let the pool.map() call return to this thread.  
-    # Another grumble.  
-    lOutput = []
-    while not qOut.empty():
-        lOutput.append(qOut.get())
-    print("---------begin cases----------")
-    print(f'Number of cases returned={len(lOutput)}')
-    for (nCaseNumber, lCase) in enumerate(lOutput):
-        print(f'Case number {nCaseNumber+1}')
-        for tCmd in lCase:
-            print(tCmd)
-        print("")
-    print("--------------")
+    # =======================================
+    # Now, they're all waiting for work.  Give them some.
+    NTRC.ntrace(3, "proc sending instructions from|%s|" % (gtInstructions))
+    fnSendInstructions(gtInstructions, 0, nPoliteTimerMsec)    
+
+    # Wait some decent interval before killing all the workers.
+#    time.sleep(10)
+    time.sleep(5)
+    
+    # =======================================
+    # Send out suicide messages.
+    NTRC.ntrace(3, "proc sending end msgs")
+    fnSendAllEndMessages()
+    #!!!!!!!!!!!!! I should wait here for all workers to exit.
+
+    # =======================================
+    # Close up.  Make sure everything exits.  
+
+    g.qOutput.put(g.tEndOutMsg)
+    # And wait for the last output.
+    g.procOutput.join()
+    
 
     return 0
 
 
+g = CG()
+    
 if __name__ == "__main__":
     sys.exit(main())
 
